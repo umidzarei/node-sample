6b69c5bd39e6f5995b4b34c86505ca99d901973a
umidzarei <umidzarei@gmail.com>
2023-07-17 20:32:29 +0330

    asdasd

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import HTTP from 'http';
 import express from 'express';
+import dotenv from 'dotenv';
+
 const app = express();
+dotenv.config();
+
 
 app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
-
-var server = app.listen('localhost', 7041, function () {
+var server = app.listen(process.env.PORT, function () {
     var host = server.address().address
     var port = server.address().port
 
